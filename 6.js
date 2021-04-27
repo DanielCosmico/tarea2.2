3a91833564aaa2f2136425606abd3f70fac0a005
Daniel Lopez <a315120@uach.mx>
2021-04-27 14:14:34 -0500

    corrigiendo el ejercicio 6 para que funcione solo con js

---
+++ 6.js
@@ -0,0 +1,12 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+   
+readline.question('Cuanto numeros quieres imprimir?', x => {
+    x++;
+    for (let i = 1; i < x; i++) {
+        console.log(i);
+    }
+    readline.close();
+});
--- 6/6.js
+++
@@ -1,6 +0,0 @@
-var x = window.prompt("Ingresa un numero: ");
-x++;
-
-for (let i = 1; i < x; i++) {
-    console.log(i);
-}
