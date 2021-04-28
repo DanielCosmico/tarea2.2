a6548635f07d212a6953e46c630c4bf7afbd2240
Daniel Lopez <a315120@uach.mx>
2021-04-27 23:29:33 -0500

    imprime un arreglo de 20 nombre en diferentes renglones

---
+++ 32.js
@@ -0,0 +1,7 @@
+let nombres = ['Daniel', 'Ruben', 'Carlos', 'Doroteo', 'Bri', 'Fani',
+            'Santiago', 'Pepe', 'Jose', 'Omar', 'Luis', 'Ivan', 'Ivana',
+            'Anahi', 'Roxana', 'Luisa', 'Luz', 'Rafael', 'Rosy', 'Lulu'];
+
+for (let i = 0; i < 20; i++) {
+    console.log(nombres[i]);
+}
